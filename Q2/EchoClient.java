795c01399af3ced7d8148d4db4b11256d46d503f
justinnguyen22 <80489273+justinnguyen22@users.noreply.github.com>
2022-02-27 13:19:33 -0800

    Add comments and replace client
    Added comments and added implementation for server to reply with server as opposed to cleint.

--- Q2/EchoClient.java
+++ Q2/EchoClient.java
@@ -1,7 +1,5 @@
-/**
- * An echo client. The client enters data to the server, and the
- * server echoes the data back to the client.
- */
+package Q2;
+
 import java.net.*;
 import java.util.Scanner;
 import java.io.*;
@@ -12,20 +10,29 @@ public class EchoClient
 
 		try {
 			System.out.println("Looking for Server...");
+
+			// Get localhost IP address using InetAddress built in function
 			InetAddress localAddress = InetAddress.getLocalHost();
+
+			// Create socket for client
 			Socket clientSocket = new Socket(localAddress, 6017);
+
 			PrintWriter pr = new PrintWriter(clientSocket.getOutputStream(), true);
 			InputStreamReader isr = new InputStreamReader(clientSocket.getInputStream());
 			BufferedReader br = new BufferedReader(isr);
 
 			System.out.println("Connected to server");
+
+			// Read in user input through terminal
 			Scanner scanner = new Scanner(System.in);
 
 			while (true) {
 				System.out.print("Input: ");
 				String input = scanner.nextLine();
+				input = input.replace("client", "server");				
 				String exit = "exit";
 
+				//If user types "exit", break from while loop. Else, print string from BufferedReader
 				if (exit.equalsIgnoreCase(input)) {
 					break;
 				} else {
@@ -35,11 +42,12 @@ public class EchoClient
 				
 			}
 			
+		// Close sockets
 		clientSocket.close();
 		scanner.close();
 
 		} catch (IOException ex) {
-			// Handle exceptions
+			System.out.println("Error");
 		}
 	}
 }
--- Q2/EchoServer.java
+++ Q2/EchoServer.java
@@ -1,3 +1,4 @@
+package Q2;
 
 import java.net.*;
 import java.io.*;
@@ -7,30 +8,36 @@ public class  EchoServer
     public static void main(String args[]) {
         System.out.println("Starting echo server...");
 
+        // Try to create a server socket on port 6017; if fail, return exception
         try (ServerSocket serverSocket = new ServerSocket(6017)){
 
             System.out.println("Waiting for client connection...");
 
+            // Create a client socket that accepts a server socket
             Socket clientSocket = serverSocket.accept();     
             System.out.println("Client connected");
 
             InputStreamReader isr = new InputStreamReader(clientSocket.getInputStream());
             BufferedReader br = new BufferedReader(isr);
             PrintWriter pw = new PrintWriter(clientSocket.getOutputStream(), true);
+
+            // Read in next line from buffered reader 
             String line = br.readLine();
 
+            // while bufferedreader does not read a null terminator
             while (line != null) {
                 System.out.println("Server: " + line);
                 pw.println(line);
                 line = br.readLine();
             }
             
+            // close sockets
             clientSocket.close();
             serverSocket.close();
             
         }
         catch(Exception e) {
-            
+            System.out.println("Error");
         }
     }
 }
